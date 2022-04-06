653fc12468c34a19c2d4f801387710588f2cfaf6
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-04-07 00:32:40 +0300

    initial Commit

---
+++ PresentationOfCities/City.swift
@@ -0,0 +1,23 @@
+//
+//  City.swift
+//  PresentationOfCities
+//
+//  Created by Furkan Açıkgöz on 16.03.2022.
+//
+
+import Foundation
+
+class City {
+    
+    var name : String
+    var image : String
+    var region : String
+    
+    init(name : String, image : String, region : String) {
+        self.name = name
+        self.image = image
+        self.region = region
+        
+    }
+    
+}
--- PresentationOfCities/ViewController.swift
+++ PresentationOfCities/ViewController.swift
@@ -7,13 +7,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    var cities = [City]()
+    var selectedCity : City?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        cities.append(City(name: "Antalya", image: "antalya", region: "Akdeniz"))
+        cities.append(City(name: "Ankara", image: "ankara", region: "İç Anadolu"))
+        cities.append(City(name: "Aksaray", image: "aksaray", region: "İç Anadolu"))
+        
+        
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = cities[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedCity = cities[indexPath.row]
+        
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            cities.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsViewController
+            destinationVC.city = selectedCity
+        }
+    }
 
 }
 
